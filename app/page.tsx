c15853b8940787aef0867bdd513d0cf2167b7d1c
felixlecat <liverpoolmax@hotmail.com>
2025-02-28 13:09:48 +0000

    create and add form details

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,53 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <main>
+      <h1>Application Form</h1>
+        <form>
+          <div>
+            <label>
+              Activity
+            </label>
+            <input type='text'>
+            </input>
+          </div>
+          <div>  
+            <label>
+              Price
+            </label>
+            <input type='number'>
+            </input>
+          </div>
+          <div>
+            <label>
+              Type
+            </label>
+            <select>
+              <option value='education'>Education</option>
+              <option value='recreational'>Recreational</option>
+              <option value='social'>Social</option>
+              <option value='diy'>DIY</option>
+              <option value='charity'>Charity</option>
+              <option value='cooking'>Cooking</option>
+              <option value='relaxation'>Relaxation</option>
+              <option value='music'>Music</option>
+              <option value='busywork'>Busywork</option>
+            </select>
+          </div>
+          <div>
+            <label>
+              Booking Required
+            </label>
+            <input type='checkbox'>
+            </input>
+          </div>
+          <div>
+            <label>
+              Accessability
+            </label>
+            <input type='range' min='0' max='10'>
+            </input>
+          </div>
+        </form>
+    </main>
+  );
 }
